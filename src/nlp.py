9c1d4e384a21e590d8bdcdfbfdfdb47edc9ecfc2
andre <andre@dsin.com.br>
2018-11-30 15:09:36 -0200

    Implementação em Pipeline

--- src/nlp.py
+++ src/nlp.py
@@ -1,172 +1,223 @@
 from sys import exit
 from pprint import pprint
 
-import numpy as np
-
-from nltk.stem import RSLPStemmer
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-
-import unicodedata
-import re
-
-from sklearn.datasets import load_iris
-
-from sklearn.model_selection import cross_val_predict, cross_validate, StratifiedKFold
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-
+from sklearn.preprocessing import FunctionTransformer
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 from sklearn.svm import SVC, LinearSVC
-
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import cross_validate
 
-from bs4 import BeautifulSoup
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import RSLPStemmer
+from nltk.stem import WordNetLemmatizer
 
+import numpy as np
+import re
+import unicodedata
 
-class nlp(object):
-    word_index_map = {}
-    current_index = 0        
-    options = {}
-    
-    def __init__(self, options = {}):
-        self.options = options
-
-        if ('remove_small_words' not in self.options):
-            self.options['remove_small_words'] = True
-
-        if ('lemmatize' not in self.options):
-            self.options['lemmatize'] = True
-
-        if ('remove_stop_words' not in self.options):
-            self.options['remove_stop_words'] = True
-
-        if ('remove_punctuation' not in self.options):
-            self.options['remove_punctuation'] = True
-
-        if ('remove_number' not in self.options):
-            self.options['remove_number'] = True
-
-        if ('language' not in self.options):
-            self.options['language'] = 'portuguese'
-        
-        self.word_index_map = {}
-        self.current_index = 0   
-        
-    def get_tokens(self, data, key_text = "text"):
-        if (self.options['language'] == 'portuguese'):
-            lemmatizer = RSLPStemmer()
-            lemmatizer = lemmatizer.stem
-            stopword = stopwords.words('portuguese')
-        else:
-            lemmatizer = WordNetLemmatizer()
-            lemmatizer = lemmatizer.lemmatize
-            stopword = stopwords.words('english')
-            
-        for item in data:
-            text = item[key_text]
-            
-            if hasattr(text, 'string'):
-                text = text.string
-
-            text = text.lower()
-            tokens = word_tokenize(text)
-
-            if (self.options['lemmatize']):
-                tokens = [lemmatizer(t) for t in tokens]                
-
-            if (self.options['remove_stop_words']):
-                tokens = [t for t in tokens if t not in stopword]
-
-            if (self.options['remove_punctuation']):
-                tokens = [self.remove_punctuation(t) for t in tokens]
-
-            if (self.options['remove_number']):
-                tokens = [self.remove_number(t) for t in tokens]
+class Tokenizer(FunctionTransformer):
                 
-            if (self.options['remove_small_words']):
-                tokens = [t for t in tokens if len(t) > 3]
+    def fit(self, X, y=None):
+        return self
 
-            for token in tokens:
-                if token not in self.word_index_map:
-                    self.word_index_map[token] = self.current_index
-                    self.current_index += 1
+    def transform(self, corpus):
+        size = len(corpus)
+        for i in range(0,size):
+            item = corpus[i]
+            if (isinstance(item, str)):
+                item = item.strip()
+                item = item.lower()
+                item = word_tokenize(item)
+                corpus[i] = item
+            
+        return corpus
 
-            item['tokens'] = tokens
-
-        return data
-
-    def get_data(self, array, key_token = "tokens", key_label = "label"):
-        data = np.zeros((len(array), len(self.word_index_map) + 1))
-        i = 0
+class Remove_Numbers(FunctionTransformer):
         
-        for item in array:
-            row = self.tokens_to_numeric(item[key_token], item[key_label])
-            data[i,:] = row
-            i += 1
+    def fit(self, X, y=None):
+        return self
 
-        np.random.shuffle(data)
+    def transform(self, corpus):
+        size = len(corpus)
+        for i in range(0,size):
+            item = corpus[i]
+            item = [Remove_Numbers.remove(t) for t in item]
+            item = list(filter(None, item))
+            corpus[i] = item
         
-        return data
+        return corpus
+
+    @staticmethod 
+    def remove(word):
+        return re.sub('[0-9\\\]', '', word) 
+
+class Remove_Small_Words(FunctionTransformer):
+
+    min_length = {}
     
-    def tokens_to_numeric(self, tokens, label):
-        x = np.zeros(len(self.word_index_map) + 1)
-        for t in tokens:
-            i = self.word_index_map[t]
-            x[i] += 1
-        x = x / x.sum()
-        x[-1] = label
-        return x
+    def __init__(self, min_length = 3):
+        self.min_length = min_length
+        
+    def fit(self, X, y=None):
+        return self
 
-    def remove_punctuation(self, word):
+    def transform(self, corpus):
+        size = len(corpus)
+        for i in range(0,size):
+            item = corpus[i]
+            item = [t for t in item if len(t) > self.min_length]
+            corpus[i] = item
+          
+        return corpus
+
+class Remove_Stop_Words(FunctionTransformer):
+
+    stopword = {}
+    
+    def __init__(self, language = 'english'):
+        if (language == 'portuguese'):
+            self.stopword = stopwords.words('portuguese')
+        else:
+            self.stopword = stopwords.words('english')
+                    
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, corpus):
+        size = len(corpus)
+        for i in range(0,size):
+            item = corpus[i]
+            item = [t for t in item if t not in self.stopword]
+            corpus[i] = item
+               
+        return corpus
+
+class Lemmatizer(FunctionTransformer):
+
+    lemmatizer = {}
+    
+    def __init__(self, language = 'english'):
+        if (language == 'portuguese'):
+            self.lemmatizer = RSLPStemmer().stem
+        else:
+            self.lemmatizer = WordNetLemmatizer().lemmatize
+            
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, corpus):
+        size = len(corpus)
+        for i in range(0,size):
+            item = corpus[i]
+            item = [self.lemmatizer(t) for t in item]
+            item = list(filter(None, item))
+            corpus[i] = item
+
+        return corpus
+
+class Remove_Punctuation(FunctionTransformer):
+        
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, corpus):
+        size = len(corpus)
+        for i in range(0,size):            
+            item = corpus[i]
+            item = [Remove_Punctuation.remove(t) for t in item]
+            item = list(filter(None, item))
+            corpus[i] = item
+        
+        return corpus
+
+    @staticmethod 
+    def remove(word):
         nfkd = unicodedata.normalize('NFKD', word)
-        word = u"".join([c for c in nfkd if not unicodedata.combining(c)])
-
+        word = u"".join([c for c in nfkd if not unicodedata.combining(c)])        
         return re.sub('[^a-zA-Z0-9 \\\]', '', word)
 
-    def remove_number(self, word):
-        return re.sub('[^a-zA-Z \\\]', '', word)               
+class Text_To_Numeric(FunctionTransformer):
+    word_map = {}
+    word_map_defined = False
+    
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, corpus):       
+        size = len(corpus)
         
-    def test_data(self, data, folds = 10, type_calc = 'validate'):
+        Text_To_Numeric.define_word_map(corpus)
+        
+        data = np.zeros((size, len(Text_To_Numeric.word_map)))
+        
+        for i in range(0,size):
+            tokens = corpus[i]            
+            row = Text_To_Numeric.tokens_to_numeric(tokens)
+            data[i,:] = row
+
+        return data
+
+    @staticmethod 
+    def define_word_map(corpus):
+        if (Text_To_Numeric.word_map_defined == False):
+            temp_word_map = {}
+            current_index = 0
+            size = len(corpus)
+            for i in range(0,size):
+                tokens = corpus[i]
+                for token in tokens:
+                    if token not in temp_word_map:
+                        temp_word_map[token] = current_index
+                        current_index += 1
+
+            Text_To_Numeric.word_map = temp_word_map
+            Text_To_Numeric.word_map_defined = True                
+        
+
+    @staticmethod     
+    def tokens_to_numeric(tokens):
+        x = np.zeros(len(Text_To_Numeric.word_map))
+        
+        for t in tokens:
+            if t in Text_To_Numeric.word_map:
+                i = Text_To_Numeric.word_map[t]
+                x[i] += 1
+                
+        x_sum = x.sum()
+        
+        if (x_sum > 0):
+            x = x / x_sum            
+        
+        return x
+
+class Utils(object):
+    @staticmethod
+    def test(features, labels, folds = 10, classifiers = []):
         result = {}
         
-        classifiers = [
-            'LogisticRegression',
-            'MultinomialNB',
-            'AdaBoostClassifier',
-            'SVC',
-            'LinearSVC',
-            'SVCScale',
-            'DecisionTree',
-            'RandomForest'
-        ]
+        if (len(classifiers) == 0):
+            classifiers = [
+                'LogisticRegression',
+                'MultinomialNB',
+                'AdaBoostClassifier',
+                'SVC',
+                'LinearSVC',
+                'SVCScale',
+                'DecisionTree',
+                'RandomForest'
+            ]
 
-        #classifiers = ['LinearSVC']
-
-        features = data[:,:-1]
-        real_labels = data[:,-1]
-
-        #data = load_iris()
-        #data = load_linnerud()
-
-        #features = data.data
-        #real_labels = data.target         
-        
         for classifier in classifiers:
-            model = self.instance_classifier(classifier)
+            model = Utils.get_classifier(classifier)
+            result[classifier] = Utils.calc_cross_validate(model, features, labels, folds)
 
-            if (type_calc == 'validate'):
-                result[classifier] = self.calc_cross_validate(model, features, real_labels, folds)
-            elif (type_calc == 'predict'):
-                result[classifier] = self.calc_cross_val_predict(model, features, real_labels, folds)
-            elif (type_calc == 'manual'):
-                result[classifier] = self.calc_manual(model, features, real_labels, folds)            
-            
         return result
 
-    def instance_classifier(self, classifier):
+    @staticmethod
+    def get_classifier(classifier):
         return {
             'LogisticRegression': LogisticRegression(solver = 'newton-cg', multi_class = 'multinomial'),
             'MultinomialNB': MultinomialNB(),
@@ -178,93 +229,28 @@ class nlp(object):
             'RandomForest': RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0)
         }[classifier]
 
-    def array_merge(self, arrays, balance = True):
-        if (not balance):
-            return np.concatenate(arrays)
-
-        newArrays = []
-        
-        length = min(map(len, arrays))
-    
-        for array in arrays:
-            array = array[:length]
-            newArrays.append(array)
-
-        return np.concatenate(newArrays)
-
-    def calc_manual(self, model, features, real_labels, folds):       
+    @staticmethod
+    def calc_cross_validate(model, features, labels, folds):
         result = {}
-
-        length = int(len(features) / folds)
-
-        train_features = features[:-length,]
-        train_labels = real_labels[:-length,]
-
-        test_features = features[-length:,]
-        test_labels = real_labels[-length:,]
-
-        classifier = model.fit(train_features, train_labels)
-
-        pred_labels = classifier.predict(test_features)
-
-        result['Accuracy'] = {}
-        result['Precision'] = {}
-        result['Recall'] = {}
-        result['F1'] = {}
-
-        result['Accuracy']['avg'] = accuracy_score(test_labels, pred_labels)
-        result['Precision']['avg'] = precision_score(test_labels, pred_labels, average = 'macro')
-        result['Recall']['avg'] = recall_score(test_labels, pred_labels, average = 'macro')
-        result['F1']['avg'] = f1_score(test_labels, pred_labels, average = 'macro')
-
-        return result
-    
-    def calc_cross_val_predict(self, model, features, real_labels, folds):
-        result = {}
-        
-        cv = StratifiedKFold(n_splits=folds)
-
-        pred_labels = cross_val_predict(
-            model,
-            features,
-            real_labels,
-            cv=cv
-        )
-
-        result['Accuracy'] = {}
-        result['Precision'] = {}
-        result['Recall'] = {}
-        result['F1'] = {}
-
-        result['Accuracy']['avg'] = accuracy_score(real_labels, pred_labels)
-        result['Precision']['avg'] = precision_score(real_labels, pred_labels, average = 'macro')
-        result['Recall']['avg'] = recall_score(real_labels, pred_labels, average = 'macro')
-        result['F1']['avg'] = f1_score(real_labels, pred_labels, average = 'macro')
-
-        return result
-
-    def calc_cross_validate(self, model, features, real_labels, folds):
-        result = {}
-        
-        cv = StratifiedKFold(n_splits=folds)
-        
+               
         scoring = ['accuracy', 'precision_macro', 'recall_macro', 'f1_macro']
         scores = cross_validate(
             model,
             features,
-            real_labels,
+            labels,
             scoring=scoring,
-            cv=cv
+            cv=folds
         )
         
-        result.update(self.get_metric('Accuracy', scores['test_accuracy']))
-        result.update(self.get_metric('Precision', scores['test_precision_macro']))
-        result.update(self.get_metric('Recall', scores['test_recall_macro']))
-        result.update(self.get_metric('F1', scores['test_f1_macro']))
+        result.update(Utils.get_metric('Accuracy', scores['test_accuracy']))
+        result.update(Utils.get_metric('Precision', scores['test_precision_macro']))
+        result.update(Utils.get_metric('Recall', scores['test_recall_macro']))
+        result.update(Utils.get_metric('F1', scores['test_f1_macro']))
 
         return result
 
-    def get_metric(self, metric, values):
+    @staticmethod
+    def get_metric(metric, values):
         result = {}
         result[metric] = {}
 
@@ -276,4 +262,20 @@ class nlp(object):
         result[metric]['std'] = values.std()
 
         return result
+    
+    @staticmethod
+    def array_merge(arrays, balance = True):
+        if (not balance):
+            return np.concatenate(arrays)
+
+        newArrays = []
+            
+        length = min(map(len, arrays))
         
+        for array in arrays:
+            array = array[:length]
+            newArrays.append(array)
+
+        return np.concatenate(newArrays)        
+
+
